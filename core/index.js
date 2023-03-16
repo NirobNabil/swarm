cca8bc908566079d923a8422e61e9827b906c588
shimla_2019331106 <69499680+shimlakanu@users.noreply.github.com>
2023-03-16 11:52:05 +0600

    add task processing

--- core/index.js
+++ core/index.js
@@ -3,7 +3,12 @@
 const grid = require('./gridMaking.js');
 const {input} = require('./input');
 const path = require('./shortestPath.js')
-const BotStateHandler = require('./RobotHandlers/BotStateHandler')
+const allPermutations = require('./allPermutations.js');
+const bestPathForNewTask = require('./bestPathForNewTask')
+// const allcombinations = require('./allcombinations.js');
+// const Qclass = require('./dataTypes.js');
+
+// const BotStateHandler = require('./RobotHandlers/BotStateHandler')
 // const Qclass = require('./dataTypes.js');
 
 
@@ -11,7 +16,37 @@ const BotStateHandler = require('./RobotHandlers/BotStateHandler')
 gameBoard = grid.createBoard(input.gridLength,input.gridWidth);
 grid.initShelf(input.noOfShelfs,gameBoard,input.shelfCoordinates);
 grid.displayBoard(gameBoard);
+// var dist = path.minDistance(gameBoard,0,0,0,8);
+// console.log(dist);
 
+//bestpath(prevTaskIds,currentPos,newTaskToAdd)
+var newPath = bestPathForNewTask.bestPath(
+    gameBoard,
+    {
+        tasks: [
+            {
+                initPos: {x: 5, y: 6},
+                endPos: {x: 6, y: 9}
+            },
+            {
+                initPos: {x: 5, y: 6},
+                endPos: {x: 8, y:4 }
+            }
+        ], 
+        curPos: {
+            x: 5,
+            y: 1
+        }
+    },
+    {
+        initPos: { x: 0, y: 0 },
+        endPos: {x: 5, y: 6}
+    });
+console.log(newPath);
+
+//premutations:
+// const combos = allPermutations.getCombos([1, 2,3], 3);
+// console.log(combos);
 
 // const readline = require('readline').createInterface({
 //     input: process.stdin,
@@ -23,17 +58,19 @@ grid.displayBoard(gameBoard);
 //     readline.close()
 // })
 
-const readline2 = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+// const readline2 = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
 
-readline2.question(`Enter order:`, order => {
-    order = order.split(' ').map( v => parseInt(v) )
-    var dist = path.minDistance(gameBoard, order[0], order[1], order[2], order[3]);
-    BotStateHandler.updateBotState('robot1', {path: dist});
-    // console.log(dist);
-    readline2.close();
-});
+// readline2.question(`Enter order:`, order => {
+//     order = order.split(' ').map( v => parseInt(v) )
+//     var dist = path.minDistance(gameBoard, order[0], order[1], order[2], order[3]);
+//     BotStateHandler.updateBotState('robot1', {path: dist});
+//     // console.log(dist);
+//     readline2.close();
+// });
+
+
 
 
